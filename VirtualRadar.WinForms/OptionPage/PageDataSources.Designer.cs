7d219de2a5ee2d90f20e74672fc5ec3974a2e0f3
Andrew Whewell <vradarserver@users.noreply.github.com>
2014-07-07 20:28:30 +0000

    Removed group box from data source page
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS22]$/VRS;C29657

--- VirtualRadar.WinForms/OptionPage/PageDataSources.Designer.cs
+++ VirtualRadar.WinForms/OptionPage/PageDataSources.Designer.cs
@@ -27,7 +27,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.checkBoxSearchPictureSubFolders = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.folderPictures = new VirtualRadar.WinForms.Controls.FolderControl();
@@ -37,32 +36,12 @@
             this.folderFlags = new VirtualRadar.WinForms.Controls.FolderControl();
             this.label1 = new System.Windows.Forms.Label();
             this.fileDatabaseFileName = new VirtualRadar.WinForms.Controls.FileNameControl();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.checkBoxSearchPictureSubFolders);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.folderPictures);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.folderSilhouettes);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.folderFlags);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.fileDatabaseFileName);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(705, 149);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "::AircraftData::";
             // 
             // checkBoxSearchPictureSubFolders
             // 
             this.checkBoxSearchPictureSubFolders.AutoSize = true;
-            this.checkBoxSearchPictureSubFolders.Location = new System.Drawing.Point(192, 125);
+            this.checkBoxSearchPictureSubFolders.Location = new System.Drawing.Point(198, 106);
             this.checkBoxSearchPictureSubFolders.Name = "checkBoxSearchPictureSubFolders";
             this.checkBoxSearchPictureSubFolders.Size = new System.Drawing.Size(158, 17);
             this.checkBoxSearchPictureSubFolders.TabIndex = 8;
@@ -72,7 +51,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 103);
+            this.label4.Location = new System.Drawing.Point(0, 84);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 13);
             this.label4.TabIndex = 7;
@@ -82,7 +61,7 @@
             // 
             this.folderPictures.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.folderPictures.Location = new System.Drawing.Point(192, 98);
+            this.folderPictures.Location = new System.Drawing.Point(198, 79);
             this.folderPictures.Name = "folderPictures";
             this.folderPictures.Size = new System.Drawing.Size(507, 20);
             this.folderPictures.TabIndex = 6;
@@ -90,7 +69,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 77);
+            this.label3.Location = new System.Drawing.Point(0, 58);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 13);
             this.label3.TabIndex = 5;
@@ -100,7 +79,7 @@
             // 
             this.folderSilhouettes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.folderSilhouettes.Location = new System.Drawing.Point(192, 72);
+            this.folderSilhouettes.Location = new System.Drawing.Point(198, 53);
             this.folderSilhouettes.Name = "folderSilhouettes";
             this.folderSilhouettes.Size = new System.Drawing.Size(507, 20);
             this.folderSilhouettes.TabIndex = 4;
@@ -108,7 +87,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Location = new System.Drawing.Point(0, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 3;
@@ -118,7 +97,7 @@
             // 
             this.folderFlags.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.folderFlags.Location = new System.Drawing.Point(192, 46);
+            this.folderFlags.Location = new System.Drawing.Point(198, 27);
             this.folderFlags.Name = "folderFlags";
             this.folderFlags.Size = new System.Drawing.Size(507, 20);
             this.folderFlags.TabIndex = 2;
@@ -126,7 +105,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Location = new System.Drawing.Point(0, 5);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 1;
@@ -136,7 +115,7 @@
             // 
             this.fileDatabaseFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.fileDatabaseFileName.Location = new System.Drawing.Point(192, 19);
+            this.fileDatabaseFileName.Location = new System.Drawing.Point(198, 0);
             this.fileDatabaseFileName.Name = "fileDatabaseFileName";
             this.fileDatabaseFileName.Size = new System.Drawing.Size(507, 20);
             this.fileDatabaseFileName.TabIndex = 0;
@@ -144,18 +123,24 @@
             // PageDataSources
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBoxSearchPictureSubFolders);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.folderFlags);
+            this.Controls.Add(this.folderPictures);
+            this.Controls.Add(this.fileDatabaseFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderSilhouettes);
+            this.Controls.Add(this.label2);
             this.Name = "PageDataSources";
-            this.Size = new System.Drawing.Size(705, 149);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.Size = new System.Drawing.Size(705, 129);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckBox checkBoxSearchPictureSubFolders;
         private System.Windows.Forms.Label label4;
         private Controls.FolderControl folderPictures;
