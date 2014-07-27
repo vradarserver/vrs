95f3fac1e9b05834060ee7e523167d74bd901084
Andrew Whewell <vradarserver@users.noreply.github.com>
2014-07-27 11:20:09 +0000

    Worked around problem with Equin.ApplicationFramework.BindingListView under Mono

--- VirtualRadar.WinForms/CommonBaseBehaviour.cs
+++ VirtualRadar.WinForms/CommonBaseBehaviour.cs
@@ -8,6 +8,7 @@
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+using InterfaceFactory;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -358,10 +359,16 @@ namespace VirtualRadar.WinForms
             var castList = list as IList;
             if(castList == null) throw new ArgumentException("The list must be castable to IList");
 
-            var bindingListView = new Equin.ApplicationFramework.BindingListView<T>(castList);
             var result = new BindingSource();
-            result.DataSource = bindingListView;
-            result.Sort = PropertyHelper.ExtractName<T>(sortColumn);
+
+            // Turns out the BindingListView crashes under Mono...
+            if(Factory.Singleton.Resolve<IRuntimeEnvironment>().Singleton.IsMono) {
+                result.DataSource = castList;
+            } else {
+                var bindingListView = new Equin.ApplicationFramework.BindingListView<T>(castList);
+                result.DataSource = bindingListView;
+                result.Sort = PropertyHelper.ExtractName<T>(sortColumn);
+            }
 
             return result;
         }
