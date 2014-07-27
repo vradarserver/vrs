1c1768eec67d9a3f77c38848314c2600f382916c
Andrew Whewell <vradarserver@users.noreply.github.com>
2014-07-27 11:41:23 +0000

    Added the workaround for Mono's broken DialogResult handling

--- VirtualRadar.WinForms/SettingsView.Designer.cs
+++ VirtualRadar.WinForms/SettingsView.Designer.cs
@@ -229,7 +229,6 @@
             // buttonOK
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.buttonOK.Location = new System.Drawing.Point(716, 527);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
