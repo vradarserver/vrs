800e9dcf18758d81a5e54154206d36139a6daad2
Andrew Whewell <vradarserver@users.noreply.github.com>
2015-06-17 20:56:00 +0000

    Updated copyright notice on German JavaScript strings file

--- VirtualRadar.WebSite/Site/Web/script/i18n/strings.de.js
+++ VirtualRadar.WebSite/Site/Web/script/i18n/strings.de.js
@@ -1,4 +1,4 @@
-﻿// Copyright © 2013 onwards, Andrew Whewell
+﻿// Copyright © 2013 onwards, Andrew Whewell and Annette Schaupp.
 // All rights reserved.
 //
 // Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
