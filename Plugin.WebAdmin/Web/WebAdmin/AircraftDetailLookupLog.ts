eb7c04196e5bb642ead5bd53ab10bb76e204900b
Andrew Whewell <vradarserver@users.noreply.github.com>
2016-01-17 22:47:11 +0000

    Added aircraft online lookup log page
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS22]$/VRS;C33342

---
+++ Plugin.WebAdmin/Web/WebAdmin/AircraftDetailLookupLog.ts
@@ -0,0 +1,46 @@
+﻿namespace VRS.WebAdmin.AircraftDetailLookupLog
+{
+    import ViewJson = VirtualRadar.Plugin.WebAdmin.View.AircraftOnlineLookupLog;
+
+    interface Model extends ViewJson.IViewModel_KO
+    {
+    }
+
+    export class PageHandler
+    {
+        private _Model: Model;
+
+        constructor()
+        {
+            this.refreshState();
+        }
+
+        refreshState()
+        {
+            $.ajax({
+                url: 'AircraftDetailLookupLog/GetState',
+                success: (data: IResponse<ViewJson.IViewModel>) => {
+                    this.applyState(data);
+                    setTimeout(() => this.refreshState(), 1000);
+                },
+                error: () => {
+                    setTimeout(() => this.refreshState(), 5000);
+                }
+            });
+        }
+
+        private applyState(state: IResponse<ViewJson.IViewModel>)
+        {
+            if(this._Model) {
+                ko.viewmodel.updateFromModel(this._Model, state.Response);
+            } else {
+                this._Model = ko.viewmodel.fromModel(state.Response, {
+                    arrayChildId: {
+                        '{root}.LogEntries': 'Icao'
+                    }
+                });
+                ko.applyBindings(this._Model);
+            }
+        }
+    }
+}
