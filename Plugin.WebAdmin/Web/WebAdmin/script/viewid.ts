083f2399180928fa7299c9191fbcbd23fa37101d
Andrew Whewell <vradarserver@users.noreply.github.com>
2016-07-24 11:42:41 +0000

    Fixed issue with heartbeat messages causing modal overlays to get cleared during long running operations

--- Plugin.WebAdmin/Web/WebAdmin/script/viewid.ts
+++ Plugin.WebAdmin/Web/WebAdmin/script/viewid.ts
@@ -73,7 +73,7 @@
                                 .show();
                         }
                     }
-                }, false);
+                }, false, true);
             }
         }
 
