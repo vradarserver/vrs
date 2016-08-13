b4480715243eec12dee72199dce4c0d3a593eab6
Andrew Whewell <vradarserver@users.noreply.github.com>
2016-08-13 22:18:12 +0100

    Added bare-bones weather layer plugin

---
+++ Plugin.WeatherLayer/Plugin.cs
@@ -0,0 +1,168 @@
+﻿// Copyright © 2015 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+using InterfaceFactory;
+using Newtonsoft.Json;
+using VirtualRadar.Interface;
+using VirtualRadar.Interface.Database;
+using VirtualRadar.Interface.Listener;
+using VirtualRadar.Interface.WebServer;
+using VirtualRadar.Interface.WebSite;
+using VirtualRadar.Localisation;
+
+namespace VirtualRadar.Plugin.WeatherLayer
+{
+    /// <summary>
+    /// The entry point for the plugin that can filter aircraft out of the feeds.
+    /// </summary>
+    public class Plugin : IPlugin
+    {
+        /// <summary>
+        /// The plugin's options.
+        /// </summary>
+        private Options _Options;
+
+        /// <summary>
+        /// The object that hooks us into the web site.
+        /// </summary>
+        private IWebSiteExtender _WebSiteExtender;
+
+        /// <summary>
+        /// The object that can substitute translations into double-colon-delimited place-holders
+        /// in a web page for us.
+        /// </summary>
+        private IHtmlLocaliser _HtmlLocaliser;
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string Id { get { return "VirtualRadarServer.Plugin.WeatherLayer"; } }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string PluginFolder { get; set; }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string Name { get { return WeatherLayerStrings.PluginName; } }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string Version { get { return "2.4.0"; } }
+
+        private string _Status;
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string Status
+        {
+            get { return _Status; }
+            internal set {
+                if(value != _Status) {
+                    _Status = value;
+                    OnStatusChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+        private string _StatusDescription;
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public string StatusDescription
+        {
+            get { return _StatusDescription; }
+            internal set {
+                if(value != _StatusDescription) {
+                    _StatusDescription = value;
+                    OnStatusChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public bool HasOptions { get { return true; } }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public event EventHandler StatusChanged;
+
+        /// <summary>
+        /// Raises <see cref="StatusChanged"/>.
+        /// </summary>
+        /// <param name="args"></param>
+        protected virtual void OnStatusChanged(EventArgs args)
+        {
+            EventHelper.Raise(StatusChanged, this, args);
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        /// <param name="classFactory"></param>
+        public void RegisterImplementations(IClassFactory classFactory)
+        {
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        /// <param name="parameters"></param>
+        public void Startup(PluginStartupParameters parameters)
+        {
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void GuiThreadStartup()
+        {
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void Shutdown()
+        {
+            if(_WebSiteExtender != null) {
+                _WebSiteExtender.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void ShowWinFormsOptionsUI()
+        {
+        }
+
+        /// <summary>
+        /// Applies the settings from the currently loaded options.
+        /// </summary>
+        private void ApplyOptions(Options options)
+        {
+            _Options = options;
+        }
+    }
+}
---
+++ Plugin.WeatherLayer/_PostBuild.bat
@@ -0,0 +1,78 @@
+@echo off
+
+rem This should be executed as a part of the post-build step for the plugin.
+rem The parameters are:
+rem 1) $(SolutionDir)
+rem 2) $(ConfigurationName)
+rem 3) $(TargetDir)
+rem 4) $(TargetName)
+rem 5) Name-Of-Plugin-SubFolder
+rem Ensure that the XML manifest file is set to always copy to the output directory on a build
+
+    set SLNDIR=%~1
+    set CONFDIR=%~2
+    set TARGETDIR=%~3
+    set ASSEMBLY=%~4
+    set PLUGINDIR=%~5
+
+    if "%PLUGINDIR%"=="" goto NOPARAM
+
+    set SOURCEDLL=%TARGETDIR%%ASSEMBLY%.dll
+    set SOURCEXML=%TARGETDIR%%ASSEMBLY%.xml
+    rem set SOURCEWEB=%TARGETDIR%..\..\Web
+    set PLUGINS=%SLNDIR%VirtualRadar\bin\x86\%CONFDIR%\Plugins
+    set DEST=%PLUGINS%\%PLUGINDIR%
+    set WEB=%PLUGINS%\%PLUGINDIR%\Web
+
+    if exist "%PLUGINS%" goto MKDEST
+        md "%PLUGINS%"
+
+:MKDEST
+    if exist "%DEST%" goto CPPLUGIN
+        md "%DEST%"
+
+:CPPLUGIN
+    copy "%SOURCEDLL%" "%DEST%"
+    copy "%SOURCEXML%" "%DEST%"
+
+    rem Delete and then copy the WEB folder into the plugin folder.
+    rem If your plugin has no web content then comment out or remove this block
+    if not exist "%WEB%\" goto WEBDELETED
+        rem rmdir /s /q "%WEB%"
+        rem if errorlevel 0 goto WEBDELETED
+        rem echo FAILED: Could not remove the "%WEB%" folder, errorlevel is %ERRORLEVEL%
+        rem goto :ENDBAD
+    :WEBDELETED
+        rem echo xcopy /EQYI "%SOURCEWEB%" "%WEB%"
+        rem xcopy /EQYI "%SOURCEWEB%" "%WEB%"
+        rem if not errorlevel 0 goto :WEBFAILED
+        rem echo Copied web site output to "%WEB%"
+        goto :WEBDONE
+    :WEBFAILED
+        echo FAILED: The copy of the plugin web site content failed with errorlevel %ERRORLEVEL%
+        goto :ENDBAD
+    :WEBDONE
+
+    rem You need to modify the batch by hand here to copy the languages that you have translations for
+    set COPYLANG=%TARGETDIR%..\..\_PostBuildCopyLanguage.bat
+    call "%COPYLANG%" "%SLNDIR%" "%CONFDIR%" "%TARGETDIR%" "%ASSEMBLY%" de-DE
+    call "%COPYLANG%" "%SLNDIR%" "%CONFDIR%" "%TARGETDIR%" "%ASSEMBLY%" fr-FR
+    call "%COPYLANG%" "%SLNDIR%" "%CONFDIR%" "%TARGETDIR%" "%ASSEMBLY%" pt-BR
+    call "%COPYLANG%" "%SLNDIR%" "%CONFDIR%" "%TARGETDIR%" "%ASSEMBLY%" ru-RU
+    call "%COPYLANG%" "%SLNDIR%" "%CONFDIR%" "%TARGETDIR%" "%ASSEMBLY%" zh-CN
+
+    goto :END
+
+:NOPARAM
+    echo.
+    echo Missing parameter - add this as a post-build step but remember to replace the
+    echo "YOUR_PLUGIN_NAME_HERE" part with the name of your plugin:
+    echo.
+    echo $(ProjectDir)\_PostBuild.bat "$(SolutionDir)" "$(ConfigurationName)" "$(TargetPath)" "YOUR_PLUGIN_NAME_HERE"
+    echo.
+
+:ENDBAD
+    exit /b 1
+
+:END
+    exit /b 0
