f014f40dd5fe76fd4f724f4b85d34511ce8e83d7
Andrew Whewell <vradarserver@users.noreply.github.com>
2016-08-29 21:15:56 +0100

    Fixed stalk heights Aircraft on ground could occasionally have a large non-zero altitude and this would be used

--- VirtualRadar.WebSite/Site/Web/script/vrs/aircraftPlotter.ts
+++ VirtualRadar.WebSite/Site/Web/script/vrs/aircraftPlotter.ts
@@ -1992,7 +1992,7 @@ namespace VRS
          */
         private getIconAltitudeStalkHeight(aircraft: Aircraft) : number
         {
-            var result = aircraft.altitude.val;
+            var result = aircraft.isOnGround ? 0 : aircraft.altitude.val;
             if(isNaN(result)) {
                 result = 0;
             } else {
