b156c6b4ec0b719bce5c26373f0a51a9a4ada5ff
Andrew Whewell <vradarserver@users.noreply.github.com>
2017-11-19 18:33:55 +0000

    Force all projects onto TypeScript 2.3

--- Plugin.DatabaseEditor/Web/DatabaseEditor/index.js
+++ Plugin.DatabaseEditor/Web/DatabaseEditor/index.js
@@ -1,14 +1,14 @@
 /**
-* @license Copyright © 2015 onwards, Andrew Whewell
-* All rights reserved.
-*
-* Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-*    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-*    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * @license Copyright © 2015 onwards, Andrew Whewell
+ * All rights reserved.
+ *
+ * Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 var DatabaseEditor;
 (function (DatabaseEditor) {
     var Index;
@@ -130,13 +130,13 @@ var DatabaseEditor;
         var RecordModel = (function () {
             function RecordModel() {
             }
-            RecordModel.mapping = {
-                'ICAOTypeCode': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } },
-                'OperatorIcao': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } },
-                'Registration': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } }
-            };
             return RecordModel;
         }());
+        RecordModel.mapping = {
+            'ICAOTypeCode': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } },
+            'OperatorIcao': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } },
+            'Registration': { create: function (options) { return ko.observable(options.data).extend({ uppercase: true }); } }
+        };
     })(Index = DatabaseEditor.Index || (DatabaseEditor.Index = {}));
 })(DatabaseEditor || (DatabaseEditor = {}));
 //# sourceMappingURL=index.js.map
