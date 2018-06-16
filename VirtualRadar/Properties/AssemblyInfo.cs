dbe9bb9c0e4cf34f03f9250dfd75fb1c3f9ec47d
Andrew Whewell <vradarserver@users.noreply.github.com>
2018-06-16 16:12:34 +0100

    Updated version numbers

--- VirtualRadar/Properties/AssemblyInfo.cs
+++ VirtualRadar/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.1.*")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyVersion("2.4.2.*")]
+[assembly: AssemblyFileVersion("2.4.2.0")]
