0e5e38cf86c122f2a4796999ef405ee6f9d6641a
Andrew Whewell <vradarserver@users.noreply.github.com>
2018-06-23 15:10:25 +0100

    Apply MHH's MLAT fix to the TypeScript, not the JavaScript

--- VirtualRadar.WebSite/Site/Web/script/vrs/aircraftListSorter.js
+++ VirtualRadar.WebSite/Site/Web/script/vrs/aircraftListSorter.js
@@ -147,7 +147,7 @@ var VRS;
     VRS.aircraftListSortHandlers[VRS.AircraftListSortableField.Mlat] = new VRS.AircraftListSortHandler({
         field: VRS.AircraftListSortableField.Mlat,
         labelKey: 'Mlat',
-        getNumberCallback: function (aircraft) { return aircraft.isMlat.val === undefined ? 0 : aircraft.isMlat ? 1 : 2; }
+        getNumberCallback: function (aircraft) { return aircraft.isMlat.val === undefined ? 0 : aircraft.isMlat.val ? 1 : 2; }
     });
     VRS.aircraftListSortHandlers[VRS.AircraftListSortableField.Model] = new VRS.AircraftListSortHandler({
         field: VRS.AircraftListSortableField.Model,
