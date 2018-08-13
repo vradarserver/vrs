7bd29c45d4d559dd11e179d302d8fb8d149e2093
Andrew Whewell <vradarserver@users.noreply.github.com>
2018-08-13 01:33:14 +0100

    Converted all plugins over to the PowerShell post-build script

--- _PostBuild.ps1
+++ _PostBuild.ps1
@@ -6,7 +6,7 @@
 function Usage
 {
     Write-Host 'usage: _PostBuild.ps1 -projectName <project name> -configurationName <configuration name> [-targetName <targetName>]'
-    Write-Host 'The targetName parameter is not optional for plugins'
+    Write-Host 'The targetName parameter is not optional for plugins or projects with translation files'
     Exit 1
 }
 
@@ -27,23 +27,26 @@ function Copy-File
 {
     param (
         [string] $source,
-        [string] $destFolder
+        [string] $destFolder,
+        [switch] $ignoreIfMissing
     )
 
     Write-Host ('Copying ' + $source + ' to ' + $destFolder)
 
-    if(![io.Directory]::Exists($destFolder)) {
-        Write-Host ('Creating folder ' + $destFolder)
-        $dirInfo = [io.Directory]::CreateDirectory($destFolder)
-    }
-
     if(![io.File]::Exists($source)) {
-        Write-Host ($source + ' does not exist')
-        Write-Host 'Cannot copy missing file'
-        Exit 1
-    }
+        if(!$ignoreIfMissing) {
+            Write-Host ($source + ' does not exist')
+            Write-Host 'Cannot copy missing file'
+            Exit 1
+        }
+    } else {
+        if(![io.Directory]::Exists($destFolder)) {
+            Write-Host ('Creating folder ' + $destFolder)
+            $dirInfo = [io.Directory]::CreateDirectory($destFolder)
+        }
 
-    Copy-Item $source $destFolder
+        Copy-Item $source $destFolder
+    }
 }
 
 function Delete-Folder
@@ -111,6 +114,46 @@ function Checksum-Folder
     }
 }
 
+function Copy-PluginWebFolder
+{
+    param (
+        [string] $projectWebDir,
+        [string] $destWebDir
+    )
+
+    if(![string]::IsNullOrWhiteSpace($projectWebDir)) {
+        $webDir = [io.Path]::Combine($projectDir, $projectWebDir)
+        if([io.Directory]::Exists($webDir)) {
+            $pluginWebDir = [io.Path]::Combine($pluginDir, $destWebDir);
+            Copy-Folder $webDir $pluginWebDir -recursive
+        }
+    }
+}
+
+function Copy-Translations
+{
+    param (
+        [string] $buildFolder
+    )
+    if([string]::IsNullOrWhiteSpace($targetName)) {
+        Usage
+    }
+
+    Get-ChildItem $buildFolder -Directory |
+    Where-Object {
+        $_.Name -match '[a-z]{2}\-[A-Z]{2}'
+    } |
+    ForEach-Object {
+        $sourceDll = [io.Path]::Combine($_.FullName, ($targetName + ".resources.dll"))
+        $targetDir = [io.Path]::Combine($virtualRadarDir, $_.Name)
+
+        # Note that the file might not exist if no translations have been done for the plugin but
+        # the plugin refers to a library that *does* have translations. The reference will create
+        # the language folders but there won't be any resource DLLs for the plugin in them.
+        Copy-File $sourceDll $targetDir -ignoreIfMissing
+    }
+}
+
 function PostBuild-WebSite-Project
 {
     $siteDir = [io.Path]::Combine($projectDir, 'Site')
@@ -134,7 +177,9 @@ function PostBuild-Plugin
     param (
         [string] $pluginName = '',
         [string] $projectWebDir = 'Web',
-        [string] $destWebDir = 'Web'
+        [string] $destWebDir = 'Web',
+        [string] $projectWebAdminDir = 'Web-WebAdmin',
+        [string] $destWebAdminDir = 'Web-WebAdmin'
     )
     if([string]::IsNullOrWhiteSpace($targetName)) {
         Usage
@@ -150,24 +195,27 @@ function PostBuild-Plugin
 
     $pluginsRoot =    [io.Path]::Combine($virtualRadarDir, "Plugins")
     $pluginDir =      [io.Path]::Combine($pluginsRoot, $pluginName)
-    $pluginDll =      [io.Path]::Combine($projectDir, "bin", $configurationName, ($targetName + '.dll'))
-    $pluginManifest = [io.Path]::Combine($projectDir, "bin", $configurationName, ($targetName + '.xml'))
+    $pluginBuildDir = [io.Path]::Combine($projectDir, "bin", $configurationName)
+    $pluginDll =      [io.Path]::Combine($pluginBuildDir, ($targetName + '.dll'))
+    $pluginManifest = [io.Path]::Combine($pluginBuildDir, ($targetName + '.xml'))
 
     Delete-Folder $pluginDir
 
     Copy-File $pluginDll $pluginDir
     Copy-File $pluginManifest $pluginDir
 
-    if(![string]::IsNullOrWhiteSpace($projectWebDir)) {
-        $webDir = [io.Path]::Combine($projectDir, $projectWebDir)
-        if([io.Directory]::Exists($webDir)) {
-            $pluginWebDir = [io.Path]::Combine($pluginDir, $destWebDir);
-            Copy-Folder $webDir $pluginWebDir -recursive
-        }
-    }
+    Copy-PluginWebFolder $projectWebDir $destWebDir
+    Copy-PluginWebFolder $projectWebAdminDir $destWebAdminDir
+
+    Copy-Translations -buildFolder $pluginBuildDir
 }
 
-switch($projectName.ToLower()) {
-    'virtualradar.website'              { PostBuild-WebSite-Project }
-    'plugin.basestationdatabasewriter'  { PostBuild-Plugin }
+# Main switch
+$caselessProject = $projectName.ToLower()
+if($caselessProject -eq 'virtualradar.website') {
+    PostBuild-WebSite-Project
+} elseif($caselessProject.StartsWith('plugin.')) {
+    PostBuild-Plugin
+} else {
+    Write-Host ('Need to add code for ' + $projectName + ' to the _PostBuild.ps1 script')
 }
