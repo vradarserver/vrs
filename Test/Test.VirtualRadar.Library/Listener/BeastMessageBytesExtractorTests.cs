236c616b1eede404e62ef725dcf2add0eff26bff
Andrew Whewell <vradarserver@users.noreply.github.com>
2020-01-26 21:22:08 +0000

    Fix wrong payload length on Beast binary format messages

--- Test/Test.VirtualRadar.Library/Listener/BeastMessageBytesExtractorTests.cs
+++ Test/Test.VirtualRadar.Library/Listener/BeastMessageBytesExtractorTests.cs
@@ -85,5 +85,40 @@ namespace Test.VirtualRadar.Library.Listener
             Assert.IsTrue(_Extractor.BufferSize > 0);
             Assert.IsTrue(_Extractor.BufferSize < 10240);
         }
+
+        [TestMethod]
+        public void BeastMessageBytesExtractor_Recognises_Mode_A_Messages_Have_A_Two_Byte_Payload()
+        {
+            // Issue #23 on Github from srsampson (25-Jan-2020) notes that the Beast message bytes
+            // extractor is erroneously using a four byte payload for mode A/C messages. The Beast
+            // documentation has it at 2 bytes, and that has been confirmed by looking at a dump
+            // of Beast messages I took back in 2012. The effect will be that any message following
+            // a Mode A or C message in the stream will be ignored, it skips until it sees the
+            // magic 1A that denotes the start of a message...
+
+            // This is a copy of the bytes extracted on 14/09/2012 17:26:51
+            var buffer = new byte[] {
+                // This isn't in the recording, it's just here so the extractor can tell the next 1A is safe to use
+                0x00,
+                // Initial 0x32 message
+                0x1A, 0x32, 0x01, 0x26, 0xC4, 0xD1, 0xFD, 0x55, 0x34, 0x02, 0xE1, 0x97, 0xB0, 0x08, 0x24, 0x71,
+                // 0x31 message with two byte payload
+                0x1A, 0x31, 0x01, 0x26, 0xC4, 0xD5, 0x35, 0x1B, 0x2D, 0x27, 0x56,
+                // 0x33 message - bug would have attached the leading 0x1A 0x33 to the end of the previous 0x31 message so this entire message would be ignored
+                0x1A, 0x33, 0x01, 0x26, 0xC4, 0xD8, 0x37, 0xAB, 0x16, 0x8D, 0x39, 0x4A, 0x18, 0x99, 0x11, 0xB2, 0x9C, 0xE0, 0x04, 0x8D, 0x21, 0x47, 0xB3, 
+            };
+
+            var extracted = _Extractor
+                .ExtractMessageBytes(buffer, 0, buffer.Length)
+                .Select(r => (ExtractedBytes)r.Clone())
+                .ToArray();
+
+            // If everything is working then we should get two messages out (the extractor only extracts 7 and 14 byte messages
+            // so the 0x31 message is always missing from the output). If the bug is in force then we only get the initial 0x32
+            // message, the 0x33 following the 0x31 gets skipped.
+            Assert.AreEqual(2, extracted.Length);
+            Assert.AreEqual(7, extracted[0].Length);
+            Assert.AreEqual(14, extracted[1].Length);
+        }
     }
 }
