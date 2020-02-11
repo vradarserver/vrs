9c183c904dff2218acfa76b0d1cc1095b03b2aab
Andrew Whewell <vradarserver@users.noreply.github.com>
2020-02-11 22:55:51 +0000

    Update to Owin beta 36

--- VirtualRadar.WebSite/LoopbackHost.cs
+++ VirtualRadar.WebSite/LoopbackHost.cs
@@ -27,6 +27,8 @@ namespace VirtualRadar.WebSite
     /// </summary>
     class LoopbackHost : ILoopbackHost
     {
+        internal const string HostType = "VRS.LoopbackHost";
+
         /// <summary>
         /// The pipeline that will be used to process messages.
         /// </summary>
@@ -62,7 +64,7 @@ namespace VirtualRadar.WebSite
 
             var environment = Factory.Resolve<IPipelineBuilderEnvironment>();
             environment.Properties[ApplicationStartupKey.Version] =     Constants.Version;
-            environment.Properties[ApplicationStartupKey.HostType] =    "VRS.LoopbackHost";
+            environment.Properties[ApplicationStartupKey.HostType] =    HostType;
 
             _Pipeline = pipelineBuilder.CreatePipeline(environment);
         }
