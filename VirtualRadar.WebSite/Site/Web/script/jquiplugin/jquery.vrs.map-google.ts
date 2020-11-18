4e99400acc1a8243447f56f84c0ffc6ab34f60be
Andrew Whewell <vradarserver@users.noreply.github.com>
2020-11-18 17:25:07 +0000

    Update Google Maps to 3.42

--- VirtualRadar.WebSite/Site/Web/script/jquiplugin/jquery.vrs.map-google.ts
+++ VirtualRadar.WebSite/Site/Web/script/jquiplugin/jquery.vrs.map-google.ts
@@ -1397,7 +1397,7 @@ namespace VRS
         return $.extend({
             // Google Map load options - THESE ONLY HAVE ANY EFFECT ON THE FIRST MAP LOADED ON A PAGE
             key:                null,                                   // If supplied then the Google Maps script is loaded with this API key. API keys are no longer optional for public servers but remain optional for LAN and local loopback servers.
-            version:            '3.36',                                 // The version of Google Maps to load.
+            version:            '3.42',                                 // The version of Google Maps to load.
             sensor:             false,                                  // True if the location-aware stuff is to be turned on.
             libraries:          [],                                     // The optional libraries to load.
             loadMarkerWithLabel:false,                                  // Loads the marker-with-labels library after loading Google Maps.
