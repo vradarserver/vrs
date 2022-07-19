9ea00f27c9e12f09776048f7bf01b3c5e95271d4
SnakePin <18491360+SnakePin@users.noreply.github.com>
2022-07-19 04:16:22 +0300

    Removed hardcoded paths from Build.bat
    Hardcoding paths will break if the VS is installed on a different location than the default one or if a newer VS version is used. Builds should be done through the "Developer PowerShell" that the VS installation provides.

--- Build.bat
+++ Build.bat
@@ -1,10 +1,5 @@
 @echo off
 
-set "VS2017MSB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
-set "VS2019MSB=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
-
-set                          "MSBUILD=%VS2019MSB%"
-if not exist "%MSBUILD%" set "MSBUILD=%VS2017MSB%"
 set SRC=%~dp0
 set "SOLUTION=%SRC%VirtualRadar.sln"
 set NOWARN=1570,1572,1573,1574,1584,1587,1591,1711
@@ -20,8 +15,6 @@ set X64=0
     if "%1"=="-debug"   set DEBUG=1
     if "%1"=="-x86"     set X86=1
     if "%1"=="-x64"     set X64=1
-    if "%1"=="-vs2017"  set "MSBUILD=%VS2017MSB%"
-    if "%1"=="-vs2019"  set "MSBUILD=%VS2019MSB%"
     shift
     goto :NEXTARG
 :ENDARGS
@@ -43,7 +36,7 @@ for %%C IN (%CONFIGS%) do (
     for %%P IN (%PLATFORMS%) do (
         set PLATFORM=%%P
 
-        "%MSBUILD%" "%SOLUTION%" -target:Restore,Build -property:Configuration=%%C,Platform=%%P -nowarn:%NOWARN%
+        msbuild "%SOLUTION%" -target:Restore,Build -property:Configuration=%%C,Platform=%%P -nowarn:%NOWARN%
         if ERRORLEVEL 1 goto :FAILED
     )
 )
@@ -61,7 +54,6 @@ goto :EOF
     echo Compilation failed - build is incomplete
     echo Configuration: %CONFIG%
     echo Platform:      %PLATFORM%
-    echo MSBuild:       %MSBUILD%
     echo.
     echo Build failed
     exit /b 1
