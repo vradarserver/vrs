2d6cb37e5476c33ee152faecd92d00f43004bc89
Andrew Whewell <vradarserver@users.noreply.github.com>
2022-08-18 23:25:06 +0100

    VATSIM: Remove OnPropertyChanged from options

--- Plugin.Vatsim/Options.cs
+++ Plugin.Vatsim/Options.cs
@@ -17,91 +17,36 @@ using System.Threading.Tasks;
 
 namespace VirtualRadar.Plugin.Vatsim
 {
-    class Options : INotifyPropertyChanged
+    class Options
     {
-        private bool _Enabled = true;
         /// <summary>
         /// Gets or sets a value indicating whether the plugin is downloading VATSIM states and building
         /// feeds from them.
         /// </summary>
-        public bool Enabled
-        {
-            get => _Enabled;
-            set => SetField(ref _Enabled, value, nameof(Enabled));
-        }
+        public bool Enabled { get; set; } = true;
 
-        private int _RefreshIntervalSeconds = 15;
         /// <summary>
         /// Gets or sets the number of seconds between fetches of VATSIM data. Note that there is a rate limit
         /// at VATSIM of 15 seconds (as of time of writing).
         /// </summary>
-        public int RefreshIntervalSeconds
-        {
-            get => _RefreshIntervalSeconds;
-            set => SetField(ref _RefreshIntervalSeconds, value, nameof(RefreshIntervalSeconds));
-        }
+        public int RefreshIntervalSeconds { get; set; } = 15;
 
-        private bool _AssumeSlowAircraftAreOnGround = true;
         /// <summary>
         /// Gets or sets a value indicating that on-ground status is to be inferred from the speed of the
-        /// aircraft. See <see cref="SlowAircraftThresholdSpeed"/>.
+        /// aircraft. See <see cref="SlowAircraftThresholdSpeedKnots"/>.
         /// </summary>
-        public bool AssumeSlowAircraftAreOnGround
-        {
-            get => _AssumeSlowAircraftAreOnGround;
-            set => SetField(ref _AssumeSlowAircraftAreOnGround, value, nameof(AssumeSlowAircraftAreOnGround));
-        }
+        public bool AssumeSlowAircraftAreOnGround { get; set; } = true;
 
-        private int _SlowAircraftThresholdSpeedKnots = 40;
         /// <summary>
         /// Gets or sets an aircraft speed (in knots) below which aircraft are considered to be on the ground if
         /// <see cref="AssumeSlowAircraftAreOnGround"/> is true.
         /// </summary>
-        public int SlowAircraftThresholdSpeed
-        {
-            get => _SlowAircraftThresholdSpeedKnots;
-            set => SetField(ref _SlowAircraftThresholdSpeedKnots, value, nameof(SlowAircraftThresholdSpeed));
-        }
+        public int SlowAircraftThresholdSpeedKnots { get; set; } = 40;
 
-        private bool _InferModelFromModelType = true;
         /// <summary>
         /// Gets or sets a value indicating that the manufacturer and model should be inferred from the reported
         /// model type.
         /// </summary>
-        public bool InferModelFromModelType
-        {
-            get => _InferModelFromModelType;
-            set => SetField(ref _InferModelFromModelType, value, nameof(InferModelFromModelType));
-        }
-
-        /// <summary>
-        /// See interface docs.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises <see cref="PropertyChanged"/>.
-        /// </summary>
-        /// <param name="args"></param>
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) => PropertyChanged?.Invoke(this, args);
-
-        /// <summary>
-        /// Sets the field's value and raises <see cref="PropertyChanged"/>, but only when the value has changed.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="field"></param>
-        /// <param name="value"></param>
-        /// <param name="fieldName"></param>
-        /// <returns>True if the value was set because it had changed, false if the value did not change and the event was not raised.</returns>
-        protected bool SetField<T>(ref T field, T value, string fieldName)
-        {
-            var result = !EqualityComparer<T>.Default.Equals(field, value);
-            if(result) {
-                field = value;
-                OnPropertyChanged(new PropertyChangedEventArgs(fieldName));
-            }
-
-            return result;
-        }
+        public bool InferModelFromModelType { get; set; } = true;
     }
 }
