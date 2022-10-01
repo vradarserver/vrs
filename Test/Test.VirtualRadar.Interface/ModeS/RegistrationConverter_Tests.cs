8cb04a99d1cd97c8305f30764dcfc8313808fe0f
Andrew Whewell <vradarserver@users.noreply.github.com>
2022-10-02 00:26:07 +0100

    WIP registration to ICAO converter

---
+++ Test/Test.VirtualRadar.Interface/ModeS/RegistrationConverter_Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Framework;
+using VirtualRadar.Interface.ModeS;
+
+namespace Test.VirtualRadar.Interface.ModeS
+{
+    [TestClass]
+    public class RegistrationConverter_Tests
+    {
+        [TestMethod]
+        public void RegistrationToModeS_Can_Convert_US_Registrations()
+        {
+            new InlineDataTest(this).TestAndAssert(new dynamic[] {
+                new { Registration = "N1",    Expected = "A00001", },
+                new { Registration = "N1A",   Expected = "A00002", },
+                new { Registration = "N1AA",  Expected = "A00003", },
+                new { Registration = "N1AZ",  Expected = "A0001A", },
+                new { Registration = "N1B",   Expected = "A0001B", },
+                new { Registration = "N1BA",  Expected = "A0001C", },
+                new { Registration = "N1BZ",  Expected = "A00033", },
+                new { Registration = "N1C",   Expected = "A00034", },
+                new { Registration = "N1HZ",  Expected = "A000C9", },
+                new { Registration = "N1J",   Expected = "A000CA", },
+                new { Registration = "N1NZ",  Expected = "A00146", },
+                new { Registration = "N1P",   Expected = "A00147", },
+                new { Registration = "N1ZZ",  Expected = "A00259", },
+                new { Registration = "N10",   Expected = "A0025A", },
+                new { Registration = "N10A",  Expected = "A0025B", },
+//                new { Registration = "N10AA", Expected = "A0025C", },
+            },
+            row => {
+                var actual = RegistrationConverter.RegistrationToModeS(row.Registration);
+                Assert.AreEqual(row.Expected, actual);
+            });
+        }
+    }
+}
---
+++ VirtualRadar.Interface/ModeS/RegistrationConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRadar.Interface.ModeS
+{
+    /// <summary>
+    /// Converts between a Mode-S code and a registration. Only a handful of
+    /// countries use an algorithm to convert between registration and Mode-S
+    /// ICAO code, and even fewer publish the algorithm, so this cannot be
+    /// used to figure out registrations for all aircraft.
+    /// </summary>
+    public static class RegistrationConverter
+    {
+        /// <summary>
+        /// Returns null if the registration cannot be converted
+        /// to an ICAO, otherwise returns the ICAO formatted as
+        /// a six digit hex string.
+        /// </summary>
+        /// <param name="registration"></param>
+        /// <returns></returns>
+        public static string RegistrationToModeS(string registration)
+        {
+            var icao = 0;
+
+            for(var idx = 1;idx < registration.Length;++idx) {
+                var ch = registration[idx];
+                switch(idx - 1) {
+                    case 0:
+                        icao = 1;
+                        break;
+                    case 1:
+                        var posnValue = Base64ATo9(ch);
+                        icao += posnValue + (24 * (posnValue - 1));
+                        break;
+                    case 2:
+                        icao += Base64ATo9(ch);
+                        break;
+                }
+            }
+
+            return (icao + (0xA00000)).ToString("X6");
+        }
+
+        static int Base64ATo9(char ch)
+        {
+            // Values are:
+            // A = 1
+            // B = 2 (etc.)
+            // H = 8
+            // (skip I)
+            // J = 9 (etc.)
+            // N = 13
+            // ... (skip O)
+            // P = 14 (etc.)
+            // Z = 24
+            // 0 = 25 (etc.)
+            // 9 = 34
+            //
+            // Unknown characters return 0, charset is always ASCII
+
+            return (ch >= '0' && ch <= '9')
+                ? (ch - '0') + 25
+                : (ch < 'A' || ch > 'Z')
+                    ? 0
+                    : (ch - 'A') + (ch < 'I' ? 1 : ch < 'O' ? 0 : -1);
+        }
+    }
+}
