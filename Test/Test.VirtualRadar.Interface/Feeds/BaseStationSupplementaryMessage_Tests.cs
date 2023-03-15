3baa89b4c37783b9248533c8d95482e416ab9184
Andrew Whewell <vradarserver@users.noreply.github.com>
2023-03-15 00:04:27 +0000

    Port BaseStationTranslatorException over

---
+++ Test/Test.VirtualRadar.Interface/Feeds/BaseStationSupplementaryMessage_Tests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright © 2012 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using VirtualRadar.Interface.Feeds;
+
+namespace Test.VirtualRadar.Interface.Feeds
+{
+    [TestClass]
+    public class BaseStationSupplementaryMessage_Tests
+    {
+        [TestMethod]
+        public void PressureSettingInHg_Returns_Correct_Values()
+        {
+            var message = new BaseStationSupplementaryMessage();
+
+            message.PressureSettingMb = null;
+            Assert.AreEqual(null, message.PressureSettingInHg);
+
+            message.PressureSettingMb = 0;  // indicates that pressure setting is outside bounds
+            Assert.AreEqual(null, message.PressureSettingInHg);
+
+            message.PressureSettingMb = 1013.25F;
+            Assert.AreEqual(29.92, (double)message.PressureSettingInHg, 0.01);
+        }
+    }
+}
