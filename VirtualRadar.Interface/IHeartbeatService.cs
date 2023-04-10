e7c69387efd852dcc8fd8c9cf677864a18bec3f4
Andrew Whewell <vradarserver@users.noreply.github.com>
2023-04-10 16:19:23 +0100

    Port heartbeat service

---
+++ VirtualRadar.Interface/IHeartbeatService.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © 2010 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+namespace VirtualRadar.Interface
+{
+    /// <summary>
+    /// A service that raises an event on a background thread every so-many minutes.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The intention with this is to make it a bit easier to write tests for code that checks for
+    /// program updates / new versions of files once every day or so. It can get complicated if the
+    /// code that does the checking is using a real timer or is invoking code on a second thread.
+    /// </para><para>
+    /// If an object wants to periodically do some work then it hooks either <see cref="SlowTick"/>
+    /// or <see cref="FastTick"/> on the object exposed by the Singleton property. These events are
+    /// raised on a background thread so the object that hooks them does not have to get involved with
+    /// maintaining a background thread or with periodically running a method on a background thread.
+    /// However none of the Tick events are <em>guaranteed</em> to be raised exactly so-many seconds
+    /// apart so the object that hooks them is expected to keep a track of the time that has elapsed
+    /// since the last invocation of the event handler and make sure that they don't perform their
+    /// periodic work before the requisite period of real time has elapsed.
+    /// </para><para>
+    /// Exceptions raised on the event handlers are currently logged but not pushed up to the GUI.
+    /// </para><para>
+    /// The Singleton version of the heartbeat service is started by the splash screen and is available
+    /// to plugins in their Startup method. However if plugins want to maintain use their own instance
+    /// of the heartbeat service they are welcome to do so - by doing this they can avoid blocking
+    /// other objects that use the service. However if timely background processing is an issue for a
+    /// plugin then perhaps the heartbeat service is not the best way of implementing it.
+    /// </para></remarks>
+    public interface IHeartbeatService : IDisposable
+    {
+        /// <summary>
+        /// Raised once every ten seconds or so.
+        /// </summary>
+        event EventHandler SlowTick;
+
+        /// <summary>
+        /// Raised once every second or so.
+        /// </summary>
+        event EventHandler FastTick;
+
+        /// <summary>
+        /// Starts the timers.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Raises <see cref="SlowTick"/> on a background thread.
+        /// </summary>
+        void SlowTickNow();
+
+        /// <summary>
+        /// Raises <see cref="FastTick"/> on a background thread.
+        /// </summary>
+        void FastTickNow();
+    }
+}
---
+++ VirtualRadar.Library/HeartbeatService.cs
@@ -0,0 +1,189 @@
+﻿// Copyright © 2010 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System.Timers;
+using VirtualRadar.Interface;
+
+namespace VirtualRadar.Library
+{
+    /// <summary>
+    /// The default implementation of <see cref="IHeartbeatService"/>.
+    /// </summary>
+    sealed class HeartbeatService : IHeartbeatService
+    {
+        private readonly ILog _Log;
+
+        /// <summary>
+        /// An enumeration of the different ticks that the Heartbeat service exposes.
+        /// </summary>
+        enum TickType
+        {
+            SlowTick,
+            FastTick,
+        }
+
+        /// <summary>
+        /// The timer that will be used to drive the <see cref="SlowTick"/> event.
+        /// </summary>
+        private System.Timers.Timer _SlowTickTimer;
+
+        /// <summary>
+        /// The timer that will be used to drive the <see cref="FastTick"/> event.
+        /// </summary>
+        private System.Timers.Timer _FastTickTimer;
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public event EventHandler SlowTick;
+
+        /// <summary>
+        /// Raises <see cref="SlowTick"/>.
+        /// </summary>
+        /// <param name="args"></param>
+        private void OnSlowTick(EventArgs args)
+        {
+            EventHelper.Raise(SlowTick, this, args, ex => {
+                _Log.WriteLine($"Caught an exception on a slow tick heartbeat event: {ex}");
+            });
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public event EventHandler FastTick;
+
+        /// <summary>
+        /// Raises <see cref="FastTick"/>.
+        /// </summary>
+        /// <param name="args"></param>
+        private void OnFastTick(EventArgs args)
+        {
+            EventHelper.Raise(FastTick, this, args, ex => {
+                _Log.WriteLine($"Caught an exception on a fast tick heartbeat event: {ex}");
+            });
+        }
+
+        /// <summary>
+        /// Creates a new object.
+        /// </summary>
+        /// <param name="log"></param>
+        public HeartbeatService(ILog log)
+        {
+            _Log = log;
+        }
+
+        /// <summary>
+        /// Finalises the object.
+        /// </summary>
+        ~HeartbeatService()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes of or finalises the object.
+        /// </summary>
+        /// <param name="disposing"></param>
+        private void Dispose(bool disposing)
+        {
+            if(disposing) {
+                if(_SlowTickTimer != null) {
+                    _SlowTickTimer.Dispose();
+                }
+                if(_FastTickTimer != null) {
+                    _FastTickTimer.Dispose();
+                }
+                _SlowTickTimer = _FastTickTimer = null;
+            }
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void Start()
+        {
+            if(_SlowTickTimer == null) {
+                _SlowTickTimer = new System.Timers.Timer(10000) { AutoReset = false };
+                _SlowTickTimer.Elapsed += SlowTickTimer_Elapsed;
+
+                _SlowTickTimer.Start();
+            }
+
+            if(_FastTickTimer == null) {
+                _FastTickTimer = new System.Timers.Timer(1000) { AutoReset = false };
+                _FastTickTimer.Elapsed += FastTickTimer_Elapsed;
+
+                _FastTickTimer.Start();
+            }
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void SlowTickNow()
+        {
+            ThreadPool.QueueUserWorkItem(BackgroundThreadRaiseTick, TickType.SlowTick);
+        }
+
+        /// <summary>
+        /// See interface docs.
+        /// </summary>
+        public void FastTickNow()
+        {
+            ThreadPool.QueueUserWorkItem(BackgroundThreadRaiseTick, TickType.FastTick);
+        }
+
+        /// <summary>
+        /// Called on a background thread, raises <see cref="SlowTick"/>.
+        /// </summary>
+        /// <param name="state"></param>
+        private void BackgroundThreadRaiseTick(object state)
+        {
+            var tickType = (TickType)state;
+            switch(tickType) {
+                case TickType.SlowTick:     SlowTickTimer_Elapsed(this, null); break;
+                case TickType.FastTick:     FastTickTimer_Elapsed(this, null); break;
+                default:                    throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Raised every time the slow tick timer elapses.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SlowTickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            OnSlowTick(EventArgs.Empty);
+            _SlowTickTimer.Start();
+        }
+
+        /// <summary>
+        /// Raised every time the fast tick timer elapses.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FastTickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            OnFastTick(EventArgs.Empty);
+            _FastTickTimer.Start();
+        }
+    }
+}
