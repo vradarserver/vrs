faa9b2d0e790964bcefa835020f07460bfe8a248
Andrew Whewell <vradarserver@users.noreply.github.com>
2023-06-17 15:57:31 +0100

    Port more bits over to extension methods

--- VirtualRadar.Database/BaseStationDatabase_Query.cs
+++ VirtualRadar.Database/BaseStationDatabase_Query.cs
@@ -27,7 +27,7 @@ namespace VirtualRadar.Database
                     ? query.Where(r => alternates.Contains(r.Callsign))
                     : query.Where(r => !alternates.Contains(r.Callsign));
             } else {
-                query = ApplyStringFilter(query, criteria.Callsign,
+                query = query.WhereStringFilter(criteria.Callsign,
                     q => q.Where(r => r.Callsign == criteria.Callsign.Value),
                     q => q.Where(r => r.Callsign != null && r.Callsign != criteria.Callsign.Value),
                     q => q.Where(r => (r.Callsign ?? "") == ""),
@@ -40,7 +40,7 @@ namespace VirtualRadar.Database
                     q => q.Where(r => r.Callsign != null && !r.Callsign.EndsWith(criteria.Callsign.Value))
                 );
             }
-            query = ApplyStringFilter(query, criteria.Country,
+            query = query.WhereStringFilter(criteria.Country,
                 q => q.Where(r => r.Aircraft.ModeSCountry == criteria.Country.Value),
                 q => q.Where(r => r.Aircraft.ModeSCountry != null && r.Aircraft.ModeSCountry != criteria.Country.Value),
                 q => q.Where(r => (r.Aircraft.ModeSCountry ?? "") == ""),
@@ -52,21 +52,21 @@ namespace VirtualRadar.Database
                 q => q.Where(r => r.Aircraft.ModeSCountry != null && r.Aircraft.ModeSCountry.EndsWith(criteria.Country.Value)),
                 q => q.Where(r => r.Aircraft.ModeSCountry != null && !r.Aircraft.ModeSCountry.EndsWith(criteria.Country.Value))
             );
-            query = ApplyDateRangeFilter(query, criteria.Date,
+            query = query.WhereDateRangeFilter(criteria.Date,
                 q => q.Where(r => r.StartTime >= criteria.Date.LowerValue.Value),
                 q => q.Where(r => r.StartTime <= criteria.Date.UpperValue.Value),
                 q => q.Where(r => r.StartTime < criteria.Date.LowerValue.Value),
                 q => q.Where(r => r.StartTime > criteria.Date.UpperValue.Value),
                 q => q.Where(r => r.StartTime < criteria.Date.LowerValue.Value || r.StartTime > criteria.Date.UpperValue.Value)
             );
-            query = ApplyIntRangeFilter(query, criteria.FirstAltitude,
+            query = query.WhereIntRangeFilter(criteria.FirstAltitude,
                 q => q.Where(r => r.FirstAltitude >= criteria.FirstAltitude.LowerValue.Value),
                 q => q.Where(r => r.FirstAltitude <= criteria.FirstAltitude.UpperValue.Value),
                 q => q.Where(r => r.FirstAltitude < criteria.FirstAltitude.LowerValue.Value),
                 q => q.Where(r => r.FirstAltitude > criteria.FirstAltitude.UpperValue.Value),
                 q => q.Where(r => r.FirstAltitude < criteria.FirstAltitude.LowerValue.Value || r.FirstAltitude > criteria.FirstAltitude.UpperValue.Value)
             );
-            query = ApplyStringFilter(query, criteria.Icao,
+            query = query.WhereStringFilter(criteria.Icao,
                 q => q.Where(r => r.Aircraft.ModeS == criteria.Icao.Value),
                 q => q.Where(r => r.Aircraft.ModeS != criteria.Icao.Value),
                 q => q.Where(r => (r.Aircraft.ModeS ?? "") == ""),
@@ -78,18 +78,18 @@ namespace VirtualRadar.Database
                 q => q.Where(r => r.Aircraft.ModeS.EndsWith(criteria.Icao.Value)),
                 q => q.Where(r => !r.Aircraft.ModeS.EndsWith(criteria.Icao.Value))
             );
-            query = ApplyBoolFilter(query, criteria.IsEmergency,
+            query = query.WhereBoolFilter(criteria.IsEmergency,
                 q => q.Where(r => r.HadEmergency == criteria.IsEmergency.Value),
                 q => q.Where(r => r.HadEmergency != criteria.IsEmergency.Value)
             );
-            query = ApplyIntRangeFilter(query, criteria.LastAltitude,
+            query = query.WhereIntRangeFilter(criteria.LastAltitude,
                 q => q.Where(r => r.LastAltitude >= criteria.LastAltitude.LowerValue.Value),
                 q => q.Where(r => r.LastAltitude <= criteria.LastAltitude.UpperValue.Value),
                 q => q.Where(r => r.LastAltitude < criteria.LastAltitude.LowerValue.Value),
                 q => q.Where(r => r.LastAltitude > criteria.LastAltitude.UpperValue.Value),
                 q => q.Where(r => r.LastAltitude < criteria.LastAltitude.LowerValue.Value || r.LastAltitude > criteria.LastAltitude.UpperValue.Value)
             );
-            query = ApplyStringFilter(query, criteria.Operator,
+            query = query.WhereStringFilter(criteria.Operator,
                 q => q.Where(r => r.Aircraft.RegisteredOwners == criteria.Operator.Value),
                 q => q.Where(r => r.Aircraft.RegisteredOwners != null && r.Aircraft.RegisteredOwners != criteria.Operator.Value),
                 q => q.Where(r => (r.Aircraft.RegisteredOwners ?? "") == ""),
@@ -101,7 +101,7 @@ namespace VirtualRadar.Database
                 q => q.Where(r => r.Aircraft.RegisteredOwners != null && r.Aircraft.RegisteredOwners.EndsWith(criteria.Operator.Value)),
                 q => q.Where(r => r.Aircraft.RegisteredOwners != null && !r.Aircraft.RegisteredOwners.EndsWith(criteria.Operator.Value))
             );
-            query = ApplyStringFilter(query, criteria.Registration,
+            query = query.WhereStringFilter(criteria.Registration,
                 q => q.Where(r => r.Aircraft.Registration == criteria.Registration.Value),
                 q => q.Where(r => r.Aircraft.Registration != null && r.Aircraft.Registration != criteria.Registration.Value),
                 q => q.Where(r => (r.Aircraft.Registration ?? "") == ""),
@@ -113,7 +113,7 @@ namespace VirtualRadar.Database
                 q => q.Where(r => r.Aircraft.Registration != null && r.Aircraft.Registration.EndsWith(criteria.Registration.Value)),
                 q => q.Where(r => r.Aircraft.Registration != null && !r.Aircraft.Registration.EndsWith(criteria.Registration.Value))
             );
-            query = ApplyStringFilter(query, criteria.Type,
+            query = query.WhereStringFilter(criteria.Type,
                 q => q.Where(r => r.Aircraft.ICAOTypeCode == criteria.Type.Value),
                 q => q.Where(r => r.Aircraft.ICAOTypeCode != null && r.Aircraft.ICAOTypeCode != criteria.Type.Value),
                 q => q.Where(r => (r.Aircraft.ICAOTypeCode ?? "") == ""),
@@ -239,160 +239,9 @@ namespace VirtualRadar.Database
                 }
             }
 
-            query = query.SkipAndTake(fromRow, toRow);
+            query = query.AddSkipAndTake(fromRow, toRow);
 
             return query;
-        }
-
-        private static IQueryable<KineticFlight> ApplyStringFilter(
-            IQueryable<KineticFlight> query,
-            FilterString filter,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereEqual,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotEqual,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereIsNull,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereIsNotNull,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereContains,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotContains,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereStartsWith,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotStartsWith,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereEndsWith,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotEndsWith
-        )
-        {
-            if(filter != null) {
-                switch(filter.Condition) {
-                    case FilterCondition.Contains:
-                        query = !filter.ReverseCondition
-                            ? whereContains(query)
-                            : whereNotContains(query);
-                        break;
-                    case FilterCondition.EndsWith:
-                        query = !filter.ReverseCondition
-                            ? whereEndsWith(query)
-                            : whereNotEndsWith(query);
-                        break;
-                    case FilterCondition.StartsWith:
-                        query = !filter.ReverseCondition
-                            ? whereStartsWith(query)
-                            : whereNotStartsWith(query);
-                        break;
-                    case FilterCondition.Equals:
-                        var matchNull = String.IsNullOrEmpty(filter.Value);
-                        if(!filter.ReverseCondition) {
-                            query = matchNull
-                                ? whereIsNull(query)
-                                : whereEqual(query);
-                        } else {
-                            query = matchNull
-                                ? whereIsNotNull(query)
-                                : whereNotEqual(query);
-                        }
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
-            }
-
-            return query;
-        }
-
-        private static IQueryable<KineticFlight> ApplyBoolFilter(
-            IQueryable<KineticFlight> query,
-            FilterBool filter,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereEqual,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotEqual
-        )
-        {
-            if(filter != null && filter.Condition == FilterCondition.Equals) {
-                query = !filter.ReverseCondition
-                    ? whereEqual(query)
-                    : whereNotEqual(query);
-            }
-
-            return query;
-        }
-
-        private static IQueryable<KineticFlight> ApplyRangeFilter(
-            IQueryable<KineticFlight> query,
-            Filter filter,
-            Func<bool> filterHasLowerValue,
-            Func<bool> filterHasUpperValue,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLowerAndUpper
-        )
-        {
-            if(filter != null) {
-                var hasLower = filterHasLowerValue();
-                var hasUpper = filterHasUpperValue();
-
-                if(!filter.ReverseCondition) {
-                    if(hasLower) {
-                        query = whereLower(query);
-                    }
-                    if(hasUpper) {
-                        query = whereUpper(query);
-                    }
-                } else {
-                    if(hasLower && hasUpper) {
-                        query = whereNotLowerAndUpper(query);
-                    } else if(hasLower) {
-                        query = whereNotLower(query);
-                    } else if(hasUpper) {
-                        query = whereNotUpper(query);
-                    }
-                }
-            }
-
-            return query;
-        }
-
-        private static IQueryable<KineticFlight> ApplyDateRangeFilter(
-            IQueryable<KineticFlight> query,
-            FilterRange<DateTime> filter,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLowerAndUpper
-        )
-        {
-            return ApplyRangeFilter(
-                query,
-                filter,
-                () => filter.LowerValue != null && filter.LowerValue.Value.Year != DateTime.MinValue.Year,
-                () => filter.UpperValue != null && filter.UpperValue.Value.Year != DateTime.MaxValue.Year,
-                whereLower,
-                whereUpper,
-                whereNotLower,
-                whereNotUpper,
-                whereNotLowerAndUpper
-            );
-        }
-
-        private static IQueryable<KineticFlight> ApplyIntRangeFilter(
-            IQueryable<KineticFlight> query,
-            FilterRange<int> filter,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLower,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotUpper,
-            Func<IQueryable<KineticFlight>, IQueryable<KineticFlight>> whereNotLowerAndUpper
-        )
-        {
-            return ApplyRangeFilter(
-                query,
-                filter,
-                () => filter.LowerValue != null && filter.LowerValue.Value != int.MinValue,
-                () => filter.UpperValue != null && filter.UpperValue.Value != int.MaxValue,
-                whereLower,
-                whereUpper,
-                whereNotLower,
-                whereNotUpper,
-                whereNotLowerAndUpper
-            );
         }
     }
 }
--- VirtualRadar.Database/LinqDynamicQueryExtensions.cs
+++ VirtualRadar.Database/LinqDynamicQueryExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VirtualRadar.Interface;
 
 namespace VirtualRadar.Database
 {
@@ -16,7 +12,7 @@ namespace VirtualRadar.Database
         /// <param name="fromRow"></param>
         /// <param name="toRow"></param>
         /// <returns></returns>
-        public static IQueryable<T> SkipAndTake<T>(this IQueryable<T> query, int fromRow, int toRow)
+        public static IQueryable<T> AddSkipAndTake<T>(this IQueryable<T> query, int fromRow, int toRow)
         {
             fromRow = Math.Max(0, fromRow);
             if(fromRow > 0) {
@@ -24,6 +20,208 @@ namespace VirtualRadar.Database
             }
             if(toRow >= fromRow) {
                 query = query.Take((toRow - fromRow) + 1);
+            }
+
+            return query;
+        }
+
+        /// <summary>
+        /// Adds a where clause for the string filter passed across.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="filter"></param>
+        /// <param name="whereEqual"></param>
+        /// <param name="whereNotEqual"></param>
+        /// <param name="whereIsNull"></param>
+        /// <param name="whereIsNotNull"></param>
+        /// <param name="whereContains"></param>
+        /// <param name="whereNotContains"></param>
+        /// <param name="whereStartsWith"></param>
+        /// <param name="whereNotStartsWith"></param>
+        /// <param name="whereEndsWith"></param>
+        /// <param name="whereNotEndsWith"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static IQueryable<T> WhereStringFilter<T>(
+            this IQueryable<T> query,
+            FilterString filter,
+            Func<IQueryable<T>, IQueryable<T>> whereEqual,
+            Func<IQueryable<T>, IQueryable<T>> whereNotEqual,
+            Func<IQueryable<T>, IQueryable<T>> whereIsNull,
+            Func<IQueryable<T>, IQueryable<T>> whereIsNotNull,
+            Func<IQueryable<T>, IQueryable<T>> whereContains,
+            Func<IQueryable<T>, IQueryable<T>> whereNotContains,
+            Func<IQueryable<T>, IQueryable<T>> whereStartsWith,
+            Func<IQueryable<T>, IQueryable<T>> whereNotStartsWith,
+            Func<IQueryable<T>, IQueryable<T>> whereEndsWith,
+            Func<IQueryable<T>, IQueryable<T>> whereNotEndsWith
+        )
+        {
+            if(filter != null) {
+                switch(filter.Condition) {
+                    case FilterCondition.Contains:
+                        query = !filter.ReverseCondition
+                            ? whereContains(query)
+                            : whereNotContains(query);
+                        break;
+                    case FilterCondition.EndsWith:
+                        query = !filter.ReverseCondition
+                            ? whereEndsWith(query)
+                            : whereNotEndsWith(query);
+                        break;
+                    case FilterCondition.StartsWith:
+                        query = !filter.ReverseCondition
+                            ? whereStartsWith(query)
+                            : whereNotStartsWith(query);
+                        break;
+                    case FilterCondition.Equals:
+                        var matchNull = String.IsNullOrEmpty(filter.Value);
+                        if(!filter.ReverseCondition) {
+                            query = matchNull
+                                ? whereIsNull(query)
+                                : whereEqual(query);
+                        } else {
+                            query = matchNull
+                                ? whereIsNotNull(query)
+                                : whereNotEqual(query);
+                        }
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+
+            return query;
+        }
+
+        private static IQueryable<T> ApplyRangeFilter<T>(
+            IQueryable<T> query,
+            Filter filter,
+            Func<bool> filterHasLowerValue,
+            Func<bool> filterHasUpperValue,
+            Func<IQueryable<T>, IQueryable<T>> whereLower,
+            Func<IQueryable<T>, IQueryable<T>> whereUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLower,
+            Func<IQueryable<T>, IQueryable<T>> whereNotUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLowerAndUpper
+        )
+        {
+            if(filter != null) {
+                var hasLower = filterHasLowerValue();
+                var hasUpper = filterHasUpperValue();
+
+                if(!filter.ReverseCondition) {
+                    if(hasLower) {
+                        query = whereLower(query);
+                    }
+                    if(hasUpper) {
+                        query = whereUpper(query);
+                    }
+                } else {
+                    if(hasLower && hasUpper) {
+                        query = whereNotLowerAndUpper(query);
+                    } else if(hasLower) {
+                        query = whereNotLower(query);
+                    } else if(hasUpper) {
+                        query = whereNotUpper(query);
+                    }
+                }
+            }
+
+            return query;
+        }
+
+        /// <summary>
+        /// Adds a where clause for the date range filter passed across.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="filter"></param>
+        /// <param name="whereLower"></param>
+        /// <param name="whereUpper"></param>
+        /// <param name="whereNotLower"></param>
+        /// <param name="whereNotUpper"></param>
+        /// <param name="whereNotLowerAndUpper"></param>
+        /// <returns></returns>
+        public static IQueryable<T> WhereDateRangeFilter<T>(
+            this IQueryable<T> query,
+            FilterRange<DateTime> filter,
+            Func<IQueryable<T>, IQueryable<T>> whereLower,
+            Func<IQueryable<T>, IQueryable<T>> whereUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLower,
+            Func<IQueryable<T>, IQueryable<T>> whereNotUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLowerAndUpper
+        )
+        {
+            return ApplyRangeFilter(
+                query,
+                filter,
+                () => filter.LowerValue != null && filter.LowerValue.Value.Year != DateTime.MinValue.Year,
+                () => filter.UpperValue != null && filter.UpperValue.Value.Year != DateTime.MaxValue.Year,
+                whereLower,
+                whereUpper,
+                whereNotLower,
+                whereNotUpper,
+                whereNotLowerAndUpper
+            );
+        }
+
+        /// <summary>
+        /// Adds a where clause for the integer range filter passed across.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="filter"></param>
+        /// <param name="whereLower"></param>
+        /// <param name="whereUpper"></param>
+        /// <param name="whereNotLower"></param>
+        /// <param name="whereNotUpper"></param>
+        /// <param name="whereNotLowerAndUpper"></param>
+        /// <returns></returns>
+        public static IQueryable<T> WhereIntRangeFilter<T>(
+            this IQueryable<T> query,
+            FilterRange<int> filter,
+            Func<IQueryable<T>, IQueryable<T>> whereLower,
+            Func<IQueryable<T>, IQueryable<T>> whereUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLower,
+            Func<IQueryable<T>, IQueryable<T>> whereNotUpper,
+            Func<IQueryable<T>, IQueryable<T>> whereNotLowerAndUpper
+        )
+        {
+            return ApplyRangeFilter(
+                query,
+                filter,
+                () => filter.LowerValue != null && filter.LowerValue.Value != int.MinValue,
+                () => filter.UpperValue != null && filter.UpperValue.Value != int.MaxValue,
+                whereLower,
+                whereUpper,
+                whereNotLower,
+                whereNotUpper,
+                whereNotLowerAndUpper
+            );
+        }
+
+        /// <summary>
+        /// Adds a where clause for a boolean filter.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="filter"></param>
+        /// <param name="whereEqual"></param>
+        /// <param name="whereNotEqual"></param>
+        /// <returns></returns>
+        public static IQueryable<T> WhereBoolFilter<T>(
+            this IQueryable<T> query,
+            FilterBool filter,
+            Func<IQueryable<T>, IQueryable<T>> whereEqual,
+            Func<IQueryable<T>, IQueryable<T>> whereNotEqual
+        )
+        {
+            if(filter != null && filter.Condition == FilterCondition.Equals) {
+                query = !filter.ReverseCondition
+                    ? whereEqual(query)
+                    : whereNotEqual(query);
             }
 
             return query;
