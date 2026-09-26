7f663be1169f5e9c07bb6d036483db284f28b6c3
Andrew Whewell <vradarserver@users.noreply.github.com>
2026-09-26 16:17:22 +0100

    Update version to 2.5

--- Plugin.BaseStationDatabaseWriter/Plugin.cs
+++ Plugin.BaseStationDatabaseWriter/Plugin.cs
@@ -152,7 +152,7 @@ namespace VirtualRadar.Plugin.BaseStationDatabaseWriter
         /// <summary>
         /// See interface docs.
         /// </summary>
-        public string Version { get { return "2.4.0"; } }
+        public string Version { get { return "2.5.0"; } }
 
         private string _Status;
         /// <summary>
--- Plugin.WebAdmin/Plugin.cs
+++ Plugin.WebAdmin/Plugin.cs
@@ -84,7 +84,7 @@ namespace VirtualRadar.Plugin.WebAdmin
         /// <summary>
         /// See interface docs.
         /// </summary>
-        public string Version { get { return "2.4.1"; } }
+        public string Version { get { return "2.5.0"; } }
 
         /// <summary>
         /// See interface docs.
